f194eebf12ca5bce18019023698fb6c4ba477d8c
HelloRamo <phobos18@gmx.de>
2023-12-12 20:26:04 +0100

    index update

--- dijkstra.js
+++ dijkstra.js
@@ -162,14 +162,42 @@ const dijkstraResult = dijkstra(startNode);                                   //
 var correctDistances = result.distances;
 var userAnswers = { 'A': 0 };                                                     // creates data type dict with A set to 0
 
-// validates user input with result and sets js alert func
-function validateInput(node) {
-    var userInput = document.getElementById('distanceTo' + node).value;
 
-    if (correctDistances[node] == userInput) {
-        userAnswers[node] = userInput;
-        alert('Correct! The distance to node ' + node + ' is ' + userInput + '.');
-    } else {
-        alert('Incorrect for node ' + node + '. Try again.');
+    // Hier HTML-Elemente erstellen und Events hinzufügen
+
+    const inputField = document.createElement('input');
+    inputField.type = 'text';
+    document.body.appendChild(inputField);
+
+    const resultWindow = document.createElement('div');
+    document.body.appendChild(resultWindow);
+
+    let currentNode = 'A';
+
+    inputField.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            const inputValue = inputField.value.trim().toUpperCase();
+            const shortestDistance = dijkstra(currentNode);
+
+            if (inputValue === currentNode) {
+                resultWindow.innerText = 'Richtig!';
+                currentNode = getNextNode(); // Funktion implementieren, um den nächsten Knoten zu erhalten
+                inputField.value = '';
+            } else {
+                resultWindow.innerText = 'Falsch. Versuche es erneut.';
+            }
+        }
+    });
+
+    function getNextNode() {
+        const nodesArray = Object.keys(nodes); // Get an array of all nodes
+        const currentIndex = nodesArray.indexOf(currentNode); // Get the index of the current node
+
+        // If the current node is the last node in the array, return the first node
+        if (currentIndex === nodesArray.length - 1) {
+            return nodesArray[0];
+        } else {
+            // Otherwise, return the next node in the array
+            return nodesArray[currentIndex + 1];
+        }
     }
-}
